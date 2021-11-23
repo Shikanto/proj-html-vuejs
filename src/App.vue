b6f4b3be68646e680c21d49d88f0ebc327e5a988
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-11-23 01:21:51 +0100

    created files for section 3

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
     <main >
       <Section1 class="spacing-section"></Section1>
       <Section2 class="spacing-section"></Section2>
+      <Section3 class="spacing-section"></Section3>
+      
     </main>
   </div>
 </template>
@@ -13,11 +15,12 @@
 import Header from "./components/Header.vue"
 import Section1 from "./components/Section1.vue"
 import Section2 from "./components/Section2.vue"
+import Section3 from "./components/Section3.vue"
 
 export default {
   name: "App",
   components: {
-    Header, Section1, Section2,
+    Header, Section1, Section2, Section3,
   },
 };
 </script>
---
+++ src/components/Section3.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+<script>
+
+
+export default {
+   name:"Section3", 
+}
+</script>
+<style lang="scss">
+    
+</style>
