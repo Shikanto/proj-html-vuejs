b824cf2530cf31a32dcd86f1fe7f3823d3524c87
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-11-23 23:22:02 +0100

    done build_push

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="">
+            
+        </div>
+        <div class="div"></div>
+        <div class="div"></div>
+    </div>
+</template>
+<script>
+export default {
+    name:"Footer",
+}
+</script>
+<style lang="scss">
+    
+</style>
---
+++ src/components/HeroBot.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hero-footer">
+
+    </div>
+</template>
+<script>
+export default {
+    name:"HeroBot",
+}
+</script>
+<style lang="scss">
+    
+</style>
